3dfdccf5d11a272aeade69ace46b594abb2d868e
TATENO Hiroyuki <oden@kgc.co.jp>
2016-05-16 10:41:38 +0900

    typo modified.

--- factory_girl.rb
+++ factory_girl.rb
@@ -2,6 +2,6 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Rails.application.routes.url_helpers
-  config.include Capybara::SDL
+  config.include Capybara::DSL
 end
 
